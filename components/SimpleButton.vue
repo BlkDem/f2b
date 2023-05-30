abd1d097a337ea5d74b546b43cd3dc88fa0d96ea
BlkDem <m_sp@mail.ru>
2023-05-30 16:37:13 +0300

    prepare

---
+++ components/SimpleButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        text: {
+            type: String,
+            default: "Some Message"
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <SimpleButton
+      :text="'Press'"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+// import SimpleButton from '../components/SimpleButton.vue';
+
+export default {
+  // components: {
+  //   SimpleButton
+  // }
+}
+</script>

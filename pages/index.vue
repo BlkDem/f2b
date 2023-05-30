f9ed20d9f72c65f2bba05ec1ce6261b65667b4bf
BlkDem <m_sp@mail.ru>
2023-05-30 17:21:15 +0300

    process

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <SimpleButton
-      :text="'Press'"
-    />
+    <video class="video-bg" autoplay loop muted>
+      <source src="/assets/video/f2b.mp4" type="video/mp4"  />
+    </video>
   </div>
 </template>
 
@@ -15,3 +15,15 @@ export default {
   // }
 }
 </script>
+
+<style scoped>
+.video-bg{
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   object-fit: cover;
+   overflow: hidden;
+}
+</style>
